027f7198173c4ac18de10a6eb78186afb113f4e5
Clay7ton32 <100812513+Clay7ton32@users.noreply.github.com>
2022-04-15 20:45:55 -0500

    Created the Reservation data type to be able to make instances/variables of.  Using the comments the IDE should even give a description of the names near the var types.  Neat! - Clay

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+// import * as internal from 'stream';  this broke for me when I opened the file, or maybe I copied it and don't remember - Clay
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,46 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'DigitalParkingSolutions';
+}
+
+//should be able to import and use as a dataType  We might want to change the file it's in though...
+// import {Reservation} from /src/app/app.component.ts
+//E.G. reservation[] : Reservation; I think.
+export type Reservation = {
+  /**
+   * Name of Customer
+   */
+  name : string;
+  /**
+   * Make of the car
+   */
+  make : string;
+  /**
+   * Model of the car
+   */
+  model : string;
+  /**
+   * Color of the car
+   */
+  color : string;
+  /**
+   * Customer License Plate
+   */
+  licensePlate : string;
+  /**
+   * Is the reservation only for 1 day?
+   */
+  oneDay : boolean;
+  /**
+   * start date of reservation
+   */
+  startDate : Date | undefined;
+  /**
+   * end date of reservation
+   */
+  endDate : Date | undefined;
+  /**
+   * Spot number in the garage
+   */
+  spotNumber : number;
 }
